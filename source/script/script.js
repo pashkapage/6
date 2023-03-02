21e8c7e515253cad7f1e33c98f6f55714656fe20
pashkapage <pashkapage@gmail.com>
2023-03-02 23:33:10 +0500

    поправил js файл на событие onscroll

--- source/script/script.js
+++ source/script/script.js
@@ -1,13 +1,11 @@
-var oldScrollY = 0;
 var nav = document.getElementsByClassName("nav");
 var social__logo = document.getElementsByClassName("social__logo");
 var social__wide = document.getElementsByClassName("social__wide");
 
 window.onscroll = function() {
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  var dY = scrolled - oldScrollY;
   
-  if ( dY > 0 ){
+  if ( scrolled > 0 ){
     nav[0].style.top ="0px"; //= "nav nav-bottom";
     social__logo[0].style.display = "none";
     social__wide[0].style.display = "none";
@@ -17,6 +15,4 @@ window.onscroll = function() {
     social__wide[0].style.display = "block";
     //console.log(social__logo);
   }
-  
-  oldScrollY = scrolled;
 }
